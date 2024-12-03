7c4ddd715211cc92111d7cfa0f5725ebc0ac29a4
Caiera02 <caiocezar2807@gmail.com>
2024-12-02 21:27:58 -0300

    Atualização de link

--- Assests/Javascript/script.js
+++ Assests/Javascript/script.js
@@ -1,31 +1,31 @@
-document.getElementById('loginForm').addEventListener('submit', function(event) {
+document.getElementById('loginForm1').addEventListener('submit', function(event) {
     event.preventDefault();
     
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-
-    
     // Fetch os dados do JSON
-    // fetch ('login.json').then((response) =>{
-    //     // Transforma JSON em OBJ
-    //     return response.json();
-    // })
-    // .then(users => {
-    //     const user = users.find(user => user.usuario === username && user.pwd === password);
-    //     const messageDiv = document.getElementById('error-msg');
+    fetch ('login.json').then((response) =>{
+        // Transforma JSON em OBJ
+        return response.json();
+    })
+    .then(users => {
+        const user = users.find(user => user.usuario === username && user.pwd === password);
+        const messageDiv = document.getElementById('error-msg');
 
-    //     if (user) {
-    //         messageDiv.textContent = 'Login bem-sucedido!';
-    //         messageDiv.style.color = "green"
-    //         // Redirecionamento de tela 
-    //         alert(`${user.usuario} Seja Bem vindo a area do aluno!!`)
+        if (user) {
+            messageDiv.textContent = 'Login bem-sucedido!';
+            messageDiv.style.color = "green"
+            // Redirecionamento de tela 
+            alert(`${user.usuario} Seja Bem vindo a area do aluno!!`)
+            window.location.href= "1Planos.html"
+            document.getElementById('name').innerHTML=user.usuario
                      
-    //     } else {
-    //         messageDiv.textContent = 'Usuário ou senha inválidos.';
-    //         messageDiv.style.color = "red"
-    //     }
+        } else {
+            messageDiv.textContent = 'Usuário ou senha inválidos.';
+            messageDiv.style.color = "red"
+        }
         
-    // })
+    })
 
 });
